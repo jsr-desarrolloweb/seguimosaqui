29ccf355913cea971ee187b2a28f758bb563b0bc
Jaime <72435300+jsr-desarrolloweb@users.noreply.github.com>
2021-08-24 12:24:11 +0200

    Setting up the first steps...

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="nav">
+  <h1>Seguimos Aquí Web App</h1>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
   <router-view/>
 </template>
 
@@ -15,16 +16,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="inicio">
+    <h2>Página de Inicio - Seguimos Aquí</h2>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+   
+  }
+}
+</script>
